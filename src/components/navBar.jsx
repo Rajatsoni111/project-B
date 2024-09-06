8ecf5893de8daa59ed6bbcaa01b7d555618e52f9
Rajat205 <rishisoni1918@gmail.com>
2024-09-06 18:28:46 +0530

    changes

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -4,6 +4,12 @@ import { useState } from "react";
 
 const NavBar = () => {
     const [dark, setdark] = useState(false);
+    const [right, setRight] = useState("-100%");
+
+
+    const handleUserShower = () => {
+        setRight(0)
+    }
     return (
         <>
             <nav className="navbar" data-theme={dark ? "dark" : "light"}>
@@ -11,10 +17,19 @@ const NavBar = () => {
                     <h1>Blog.</h1>
                 </div>
                 <div className="search_bar">
-                    <input type="text" placeholder="search blogger" />
+                    <input onClick={handleUserShower} type="text" placeholder="search blogger" />
                     <SearchOutlinedIcon />
                 </div>
             </nav>
+            <div className="side-user-bar" style={{ right: right }}>
+                <div className='searched-user'>
+                    <div className='userdata'>
+                        <img src="./default-user-icon.jpg" alt="" />
+                        <p>username</p>
+                    </div>
+                    <button>follow</button>
+                </div>
+            </div>
         </>
     )
 }
